bb9a053e5033bb6622b767ce460b6b8fdd8453b9
indulekhaps <144669727+indulekhaps@users.noreply.github.com>
2023-10-04 09:22:44 +0530

    Add files via upload

---
+++ q2c.py
@@ -0,0 +1,4 @@
+V=['a','e','i','o','u']
+word=input("enter the word")
+s=[i for i in word if i in V]
+print(s)
---
+++ q5.py
@@ -0,0 +1,4 @@
+list = str(['a', 'appu', 'ramu'])
+print("list : ", list)
+count = list.count("a")
+print("Occurance of 'a' = ", count)
